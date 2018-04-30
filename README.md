fccbb6727304efa4e43d9d5d0e2b7552ec049433
Ira Miller <public@iramiller.com>
2018-04-29 21:47:43 -0500

    use constructor fs

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # js-guld-lib
 
 Javascript types for [ledger-cli](https://ledger-cli.org) data, using decimal.js for numbers.
+
+### Tests
+
+This package has good unit test coverage, and the tests are useful examples.
+
+To run:
+
+```
+npm run test
+```
--- guld-lib.js
+++ guld-lib.js
@@ -7,68 +7,68 @@
 /* global fs:false Amount:false git:false */
 
 class Blocktree {
-  constructor (fs, observer) {
+  constructor (cfs, observer) {
+    this.fs = cfs || fs
     this.observer = observer || 'guld'
   }
 
   getPrice (commodity, base) {
+    var pricefl
+    var pricea
+    var amtstr
+    var re
     commodity = commodity || 'GULD'
     base = base || 'USD'
     commodity = commodity.toUpperCase()
     base = base.toUpperCase()
+    let filterPricesByTime = line => {
+      if (!line.startsWith('P ')) return false
+      else {
+        var pdate = line.substring(2, 12)
+        var apdate = pdate.split('/')
+        pdate = `${apdate[1]}/${apdate[2]}/${apdate[0]}`
+        var now = Date.now()
+        var ptime = new Date(pdate).getTime()
+        if (now >= ptime) {
+          return true
+        } else return false
+      }
+    }
     return new Promise((resolve, reject) => {
-      fs.readFile(
+      this.fs.readFile(
         `/BLOCKTREE/${this.observer}/ledger/prices/${commodity.toLowerCase()}.db`,
         'utf-8', (err, pricef) => {
           if (err) return reject(err)
           pricef = pricef.split('\n').reverse()
-          var pricefl = pricef.filter((line) => {
-            if (!line.startsWith('P ')) return false
-            else {
-              var pdate = line.substring(2, 12)
-              var apdate = pdate.split('/')
-              pdate = `${apdate[1]}/${apdate[2]}/${
-                apdate[0]}`
-              var now = Date.now()
-              var ptime = new Date(pdate).getTime()
-              if (now >= ptime) {
-                return true
-              } else return false
-            }
-          })
-          var re = new RegExp(`${commodity.toUpperCase()} .*[0-9.].*`, 'm')
-          var pricea = re.exec(pricefl.join('\n'))
+          pricefl = pricef.filter(filterPricesByTime)
+          re = new RegExp(`${commodity.toUpperCase()} .*[0-9.].*`, 'm')
+          pricea = re.exec(pricefl.join('\n'))
           if (pricea && pricea.length > 0 && pricea[0].length > 0) {
-            var amtstr = pricea[0].replace(commodity.toUpperCase(), '').trim(
-              ' ')
+            amtstr = pricea[0].replace(commodity.toUpperCase(), '').trim()
             return resolve(new Amount(amtstr.replace(base, '').trim(), base))
           }
-          return reject(new Error(`Price not found for commodity ${
-            commodity}`))
+          return reject(new RangeError(`Price not found for commodity ${commodity}`))
         })
     })
   }
 
   listNames () {
     var namelist = []
-    return git.listFiles({
-      fs,
-      dir: `/BLOCKTREE/${this.observer}/keys/pgp`
-    }).then((keys) => {
+    var gname
+    let pushName = line => {
+      gname = line.split('/')[0]
+      if (namelist.indexOf(gname) === -1) namelist.push(gname)
+    }
+    let repo = {
+      fs: this.fs,
+      dir: `/BLOCKTREE/${this.observer}/ledger/GULD`
+    }
+    return git.listFiles(repo).then((keys) => {
       keys = keys.filter((line) => {
         return line.indexOf('/') >= 0
-      }).forEach((line) => {
-        var gname = line.split('/')[0]
-        if (namelist.indexOf(gname) === -1) namelist.push(gname)
-      })
-      return git.listFiles({
-        fs,
-        dir: `/BLOCKTREE/${this.observer}/ledger/GULD`
-      }).then((ledgers) => {
-        ledgers.forEach((line) => {
-          var gname = line.split('/')[0]
-          if (namelist.indexOf(gname) === -1) namelist.push(gname)
-        })
+      }).forEach(pushName)
+      return git.listFiles(repo).then((ledgers) => {
+        ledgers.forEach(pushName)
         return namelist
       })
     })
