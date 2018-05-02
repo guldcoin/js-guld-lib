5ca8367e17b1595167ebca85546d303da32a73b1
Ira Miller <public@iramiller.com>
2018-05-01 22:43:16 -0500

    transactions

--- guld-lib.js
+++ guld-lib.js
@@ -15,6 +15,98 @@ function mkdirps (p, tfs) {
   }
 }
 
+class Transaction {
+  constructor (text) {
+    this.raw = text
+  }
+
+  static getType (tx) {
+    var re = /^[0-9]{4}\/[0-9]{2}\/[0-9]{2} \* [ a-zA-Z0-9]*$/m
+    var txheader = re.exec(tx)
+    if (txheader && txheader.length > 0 && txheader[0].length > 0) {
+      return txheader[0].split('*')[1].trim()
+    } else {
+      throw new TypeError('expected a ledger transaction, but found unknown type')
+    }
+  }
+
+  static getTimestamp (tx) {
+    var re = /^ {4}; timestamp: [0-9]+$/m
+    var txheader = re.exec(tx)
+    if (txheader && txheader.length > 0 && txheader[0].length > 0) {
+      return txheader[0].split(':')[1].trim()
+    } else {
+      throw new TypeError('expected a ledger transaction, but found unknown type')
+    }
+  }
+
+  static getAmount (tx) {
+    var re = /^ +[:a-zA-Z-]+ +[0-9a-zA-Z,. ]+$/m
+    var txheader = re.exec(tx)
+    if (txheader && txheader.length > 0 && txheader[0].length > 0) {
+      var posting = txheader[0].replace(',', '')
+      re = /[0-9.]+/
+      txheader = re.exec(posting)
+      if (txheader && txheader.length > 0 && txheader[0].length > 0) {
+        return txheader[0]
+      } else {
+        throw new TypeError('expected a ledger transaction, but found unknown type')
+      }
+    } else {
+      throw new TypeError('expected a ledger transaction, but found unknown type')
+    }
+  }
+}
+
+class Transfer extends Transaction {
+  static create (sender, receipient, amount, commodity, time) {
+    time = time || Math.trunc(Date.now() / 1000)
+    var date = new Date(time * 1000)
+    var datestr = `${date.toISOString().split('T')[0].replace(/-/g, '/')}`
+    return new Transfer(`${datestr} * transfer
+    ; timestamp: ${time}
+    ${sender}:Assets   -${amount} ${commodity}
+    ${sender}:Expenses   ${amount} ${commodity}
+    ${receipient}:Assets   ${amount} ${commodity}
+    ${receipient}:Income   -${amount} ${commodity}
+`)
+  }
+}
+
+class Grant extends Transaction {
+  static create (contributor, amount, commodity, time) {
+    time = time || Math.trunc(Date.now() / 1000)
+    var date = new Date(time * 1000)
+    var datestr = `${date.toISOString().split('T')[0].replace(/-/g, '/')}`
+    return new Grant(`${datestr} * grant
+    ; timestamp: ${time}
+    ${contributor}:Assets   ${amount} ${commodity}
+    ${contributor}:Income   -${amount} ${commodity}
+    guld:Liabilities   -${amount} ${commodity}
+    guld:Equity:${contributor}   ${amount} ${commodity}
+`)
+  }
+}
+
+class Register extends Transaction {
+  static create (rname, rtype, qty, commodity, payee, time) {
+    var amount = qty * 0.1
+    if (payee) {
+      payee = `:${payee}`
+    } else payee = ''
+    time = time || Math.trunc(Date.now() / 1000)
+    var date = new Date(time * 1000)
+    var datestr = `${date.toISOString().split('T')[0].replace(/-/g, '/')}`
+    return new Register(`${datestr} * register ${rtype}
+    ; timestamp: ${time}
+    ${rname}:Assets   -${amount} ${commodity}
+    ${rname}:Expenses   ${amount} ${commodity}
+    guld:Liabilities   ${amount} ${commodity}
+    guld:Income:register:${rtype}:${rname}${payee}   -${amount} ${commodity}
+`)
+  }
+}
+
 class Blocktree {
   constructor (cfs, observer) {
     this.fs = cfs
@@ -45,8 +137,7 @@ class Blocktree {
     }
     return new Promise((resolve, reject) => {
       this.fs.readFile(
-        `/BLOCKTREE/${this.observer}/ledger/prices/${commodity.toLowerCase()}.db`,
-        'utf-8', (err, pricef) => {
+        `/BLOCKTREE/${this.observer}/ledger/prices/${commodity.toLowerCase()}.db`, 'utf-8', (err, pricef) => {
           if (err) return reject(err)
           else {
             pricef = pricef.split('\n').reverse()
@@ -150,6 +241,10 @@ class Blocktree {
 
 if (typeof module !== 'undefined') {
   module.exports = {
-    Blocktree: Blocktree
+    Blocktree: Blocktree,
+    Transaction: Transaction,
+    Transfer: Transfer,
+    Grant: Grant,
+    Register: Register
   }
 } // Otherwise assume we're in a browser environment
--- test.js
+++ test.js
@@ -10,8 +10,7 @@ global.Amount = ltypes.Amount
 global.Balance = ltypes.Balance
 global.Account = ltypes.Account
 global.git = require('isomorphic-git')
-const guldlib = require('./guld-lib.js')
-const Blocktree = guldlib.Blocktree
+const {Blocktree, Transaction, Transfer, Grant, Register} = require('./guld-lib.js')
 const BrowserFS = require('browserfs')
 
 function getBrowserFS (config) {
@@ -26,15 +25,170 @@ function getBrowserFS (config) {
 
 var cfs = fs
 
+const GRANT = `2016/01/01 * grant
+    ; timestamp: 1451612082
+    isysd:Assets   1.9 GULD
+    isysd:Income   -1.9 GULD
+    guld:Liabilities   -1.9 GULD
+    guld:Equity:isysd   1.9 GULD
+`
+
+const TRANSFER_GULD = `2016/01/01 * transfer
+    ; timestamp: 1451612083
+    isysd:Assets   -1.9 GULD
+    isysd:Expenses   1.9 GULD
+    cz:Assets   1.9 GULD
+    cz:Income   -1.9 GULD
+`
+
+const REGISTER_INDIVIDUAL = `2016/01/01 * register individual
+    ; timestamp: 1451612084
+    isysd:Assets   -0.1 GULD
+    isysd:Expenses   0.1 GULD
+    guld:Liabilities   0.1 GULD
+    guld:Income:register:individual:isysd   -0.1 GULD
+`
+
+const REGISTER_GROUP = `2016/01/01 * register group
+    ; timestamp: 1451612085
+    isysd:Assets   -0.1 GULD
+    isysd:Expenses   0.1 GULD
+    guld:Liabilities   0.1 GULD
+    guld:Income:register:individual:gg   -0.1 GULD
+`
+
+const REGISTER_GROUP_PAYEE = `2016/01/01 * register group
+    ; timestamp: 1451612086
+    isysd:Assets   -0.1 GULD
+    isysd:Expenses   0.1 GULD
+    guld:Liabilities   0.1 GULD
+    guld:Income:register:individual:gg:isysd   -0.1 GULD
+`
+
+describe('Transaction', () => {
+  it('getType', function () {
+    var gtype = Transaction.getType(GRANT)
+    assert(gtype === 'grant')
+    gtype = Transaction.getType(TRANSFER_GULD)
+    assert(gtype === 'transfer')
+    gtype = Transaction.getType(REGISTER_INDIVIDUAL)
+    assert(gtype === 'register individual')
+    gtype = Transaction.getType(REGISTER_GROUP)
+    assert(gtype === 'register group')
+    gtype = Transaction.getType(REGISTER_GROUP_PAYEE)
+    assert(gtype === 'register group')
+  })
+  it('getType new', function () {
+    var type = Transaction.getType(GRANT.replace('grant', 'theft'))
+    assert(type === 'theft')
+  })
+  it('getType incomplete', function () {
+    try {
+      Transaction.getType(GRANT.replace('*', ''))
+      assert(true === false)
+    } catch (e) {
+      assert(e instanceof TypeError)
+    }
+  })
+  it('getTimestamp', function () {
+    var gtime = Transaction.getTimestamp(GRANT)
+    assert(gtime === '1451612082')
+    gtime = Transaction.getTimestamp(TRANSFER_GULD)
+    assert(gtime === '1451612083')
+    gtime = Transaction.getTimestamp(REGISTER_INDIVIDUAL)
+    assert(gtime === '1451612084')
+    gtime = Transaction.getTimestamp(REGISTER_GROUP)
+    assert(gtime === '1451612085')
+    gtime = Transaction.getTimestamp(REGISTER_GROUP_PAYEE)
+    assert(gtime === '1451612086')
+  })
+  it('getAmount', function () {
+    var gamount = Transaction.getAmount(GRANT)
+    assert(gamount === '1.9')
+    gamount = Transaction.getAmount(TRANSFER_GULD)
+    assert(gamount === '1.9')
+    gamount = Transaction.getAmount(REGISTER_INDIVIDUAL)
+    assert(gamount === '0.1')
+    gamount = Transaction.getAmount(REGISTER_GROUP)
+    assert(gamount === '0.1')
+    gamount = Transaction.getAmount(REGISTER_GROUP_PAYEE)
+    assert(gamount === '0.1')
+  })
+  it('constructor', function () {
+    var trans = new Transaction(TRANSFER_GULD)
+    assert(trans instanceof Transaction)
+    assert(trans.raw === TRANSFER_GULD)
+  })
+  describe('Transfer', () => {
+    it('constructor', function () {
+      var trans = new Transfer(TRANSFER_GULD)
+      assert(trans instanceof Transfer)
+      assert(trans instanceof Transaction)
+      assert(trans.raw === TRANSFER_GULD)
+    })
+    it('create', function () {
+      var trans = Transfer.create('isysd', 'cz', 1.9, 'GULD', 1451612083)
+      assert(trans instanceof Transfer)
+      assert(trans instanceof Transaction)
+      assert(trans.raw === TRANSFER_GULD)
+    })
+    it('create no time', function () {
+      var now = Math.trunc(Date.now() / 1000)
+      var trans = Transfer.create('isysd', 'cz', 1.9, 'GULD')
+      assert(trans instanceof Transfer)
+      assert(trans instanceof Transaction)
+      var time = Transfer.getTimestamp(trans.raw)
+      assert([now.toString(), (now + 1).toString()].indexOf(time) >= 0)
+    })
+  })
+  describe('Grant', () => {
+    it('constructor', function () {
+      var trans = new Grant(GRANT)
+      assert(trans instanceof Grant)
+      assert(trans instanceof Transaction)
+      assert(trans.raw === GRANT)
+    })
+    it('create', function () {
+      var trans = Grant.create('isysd', 1.9, 'GULD', 1451612082)
+      assert(trans instanceof Grant)
+      assert(trans instanceof Transaction)
+      assert(trans.raw === GRANT)
+    })
+    it('create no time', function () {
+      var now = Math.trunc(Date.now() / 1000)
+      var trans = Grant.create('isysd', 1.9, 'GULD')
+      assert(trans instanceof Grant)
+      assert(trans instanceof Transaction)
+      var time = Grant.getTimestamp(trans.raw)
+      assert([now.toString(), (now + 1).toString()].indexOf(time) >= 0)
+    })
+  })
+  describe('Register', () => {
+    it('constructor', function () {
+      var trans = new Register(REGISTER_INDIVIDUAL)
+      assert(trans instanceof Register)
+      assert(trans instanceof Transaction)
+      assert(trans.raw === REGISTER_INDIVIDUAL)
+    })
+    it('create', function () {
+      var trans = Register.create('isysd', 'individual', 1, 'GULD', undefined, 1451612084)
+      assert(trans instanceof Register)
+      assert(trans instanceof Transaction)
+      assert(trans.raw === REGISTER_INDIVIDUAL)
+    })
+    it('create no time', function () {
+      var now = Math.trunc(Date.now() / 1000)
+      var trans = Register.create('isysd', 1.9, 'GULD')
+      assert(trans instanceof Register)
+      assert(trans instanceof Transaction)
+      var time = Register.getTimestamp(trans.raw)
+      assert([now.toString(), (now + 1).toString()].indexOf(time) >= 0)
+    })
+  })
+})
+
 describe('Blocktree', () => {
   before(done => {
-    //    var altconfig = {
-    //      fs: 'FolderAdapter',
-    //      options: {
-    //        folder: "tmp",
-    //        wrapped: fs
-    //      }
-    //    }
     getBrowserFS().then(tfs => {
       cfs = tfs
       done()
@@ -59,21 +213,21 @@ describe('Blocktree', () => {
       done()
     }).catch(done)
   })
-  it('initFS again', function (done) {
-    this.timeout(150000)
-    this.blocktree.initFS().then(() => {
-      var plist = cfs.readdirSync(`/BLOCKTREE/guld/ledger/prices`)
-      assert(plist.length > 0)
-      assert(plist.indexOf('gg.db') >= 0)
-      var glist = cfs.readdirSync(`/BLOCKTREE/guld/ledger/GULD`)
-      assert(glist.length > 0)
-      assert(glist.indexOf('isysd') >= 0)
-      var klist = cfs.readdirSync(`/BLOCKTREE/guld/keys/pgp`)
-      assert(klist.length > 0)
-      assert(klist.indexOf('isysd') >= 0)
-      done()
-    }).catch(done)
-  })
+    it('initFS again', function (done) {
+      this.timeout(150000)
+      this.blocktree.initFS().then(() => {
+        var plist = cfs.readdirSync(`/BLOCKTREE/guld/ledger/prices`)
+        assert(plist.length > 0)
+        assert(plist.indexOf('gg.db') >= 0)
+        var glist = cfs.readdirSync(`/BLOCKTREE/guld/ledger/GULD`)
+        assert(glist.length > 0)
+        assert(glist.indexOf('isysd') >= 0)
+        var klist = cfs.readdirSync(`/BLOCKTREE/guld/keys/pgp`)
+        assert(klist.length > 0)
+        assert(klist.indexOf('isysd') >= 0)
+        done()
+      }).catch(done)
+    })
   it('getPrice USD', function (done) {
     var t = this;
     (async function () {
